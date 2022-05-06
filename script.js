92cbf9c49a3b5c588bbb2041d31781b2ad3d347b
veermahajan <73316235+veermahajan@users.noreply.github.com>
2022-05-06 12:40:25 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+var x = document.getElementById("anthem");
+
+function begin(){
+  alert("Please stand up. The national anthem is about to play.");
+  alert("You may not pause the national anthem as it is playing.");
+  x.play();
+}
