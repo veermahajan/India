53a05a05afc007b9c100de947f9fbfeace609b99
veermahajan <73316235+veermahajan@users.noreply.github.com>
2022-05-06 14:39:28 -0700

    Update script.js

--- script.js
+++ script.js
@@ -4,5 +4,6 @@ function begin(){
   alert("Please stand up. The national anthem is about to play.");
   alert("You may not pause the national anthem as it is playing.");
   x.play();
+  document.getElementById("beginButton").innerHTML = "Next";
   alert("You may now sit down. Click 'Next' to continue the journey.");
 }
